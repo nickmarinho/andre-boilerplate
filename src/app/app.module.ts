831c1229e74ca11c0c4d68796b9bc083eb050910
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-08-16 07:10:48 -0300

    Adding comment instructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// import { CoreModule } from 'andre-core/dist/core.module';
+// esse é o importador do seu pack de módulos 
+
 @NgModule({
   declarations: [
     AppComponent
